ea8c17c2f26de40806480e282264f23b2964490f
Robert Kaspar <chikinn@gmail.com>
2025-02-16 02:35:20 -0800

    Fix importing so project runs on Python 3.12.7

--- players/__init__.py
+++ players/__init__.py
@@ -1,19 +1,22 @@
-"""Copied from hanabi.  This imports all players in this directory that are
-subclassed from Player, this way you can say `from players import *` or `from
-players import RandomPlayer`."""
+"""Copied from hanabi and stackoverflow.com/questions/67631.  This imports all
+players in this directory that are subclassed from Player, this way you can say
+`from players import *` or `from players import RandomPlayer`."""
 
-__all__ = []
-
-import pkgutil
-import inspect
 from bl_classes import Player
+import importlib
+import inspect
+import pkgutil
+import sys
 
-for loader, name, is_pkg in pkgutil.walk_packages(__path__):
+for finder, name, is_pkg in pkgutil.walk_packages(__path__):
     try: # only fail later if desired players can't be loaded *cough* numpy
-      module = loader.find_module(name).load_module(name)
+        spec = finder.find_spec(name)
+        module = importlib.util.module_from_spec(spec)
+        sys.modules[name] = module  # needed for exec_module to work
+        spec.loader.exec_module(module)
     except ImportError as err:
-      print("Failed to import " + name + " due to error: " + str(err))
-      continue
+        print("Failed to import " + name + " due to error: " + str(err))
+        continue
 
     for name, value in inspect.getmembers(module):
         if name.startswith('__'):
@@ -24,5 +27,4 @@ for loader, name, is_pkg in pkgutil.walk_packages(__path__):
 
         # Only add it to exports if Player subclass
         if Player in value.__subclasses__():
-            globals()[name] = value
-            __all__.append(name)
+            sys.modules[name] = importlib.import_module(name)
